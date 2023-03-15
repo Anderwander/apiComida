3bd07f8314a4dfc285324626a9aa728e36d86994
Anderwander <ander1993trabajo@hotmail.com>
2023-03-15 13:39:26 +0100

    se muestran por secciones separadas

--- js/inicio.js
+++ js/inicio.js
@@ -1,7 +1,3 @@
-let url = createBaseUrl();
-
-//url.searchParams.append("health","plant-based")
-
 function createBaseUrl() {
   let url = new URL("https://api.edamam.com/api/recipes/v2");
   url.searchParams.set("app_id", "562aebeb");
@@ -9,43 +5,90 @@ function createBaseUrl() {
   url.searchParams.set("type", "public");
   url.searchParams.set("health", "vegan");
   url.searchParams.set("cuisineType", "japanese");
+  url.searchParams.set("random", "true");
+  url.searchParams.set("limit", "5");
   return url;
 }
 
-async function getRecipes() {
+async function showStarter() {
+  let url = createBaseUrl();
+  url.searchParams.append("dishType", "starter");
+  await renderRecipe(url, "Starters");
+}
+
+async function showMain() {
+  let url = createBaseUrl();
+  url.searchParams.append("dishType", "Main course");
+  await renderRecipe(url, "Main Courses");
+}
+
+async function showDessert() {
+  let url = createBaseUrl();
+  url.searchParams.append("dishType", "desserts");
+  await renderRecipe(url, "Desserts");
+}
+
+async function showDrinks() {
+  let url = createBaseUrl();
+  url.searchParams.append("dishType", "Drinks");
+  await renderRecipe(url, "Drinks");
+}
+
+async function getRecipes(url) {
   let recipes = await fetch(url.toString())
     .then((response) => response.json())
     .then((data) => {
-      return data.hits.map((recipe) => {
-        return {
-          name: recipe.recipe.label,
-          image: recipe.recipe.image,
-          id: recipe.recipe.uri.split("_")[1],
-        };
-      });
+      return data.hits
+        .map((hit) => {
+          return {
+            name: hit.recipe.label,
+            image: hit.recipe.image,
+            id: hit.recipe.uri.split("_")[1],
+            //ingredients: hit.recipe.ingredients,
+          };
+        })
+        .slice(0, 10);
     });
+  console.log(recipes);
   return recipes;
 }
 
-async function renderRecipe() {
-  const recetas = await getRecipes();
+async function renderRecipe(url, nombre) {
+  const recipes = await getRecipes(url);
   const results = document.getElementById("results");
-  results.innerHTML = "";
-  recetas.forEach((recetas) => {
+  const food = document.createElement("section");
+  const title = document.createElement("h1");
+  title.innerText = nombre;
+  food.setAttribute("id", "sectionFood");
+  results.appendChild(title);
+  results.appendChild(food);
+
+  recipes.forEach((hit) => {
     let recipeArticle = document.createElement("article");
-    recipeArticle.classList.add("recipe");
+    recipeArticle.style.display = "none";
     let recipeName = document.createElement("h2");
-    recipeName.innerText =
-      "Vegan " + recetas.name.replace(/\srecipe[s]?/gim, "");
     let recipeImage = document.createElement("img");
-    recipeImage.setAttribute("src", recetas.image);
+    recipeImage.onload = function () {
+      recipeArticle.style.display = "block";
+    };
+
+    recipeArticle.classList.add("recipe");
+    recipeName.innerText = hit.name
+      .replace(/\srecipe[s]?/gim, "")
+      .replace(/,[s]*/g, ", ");
+    recipeImage.setAttribute("src", hit.image);
+
+    food.appendChild(recipeArticle);
     recipeArticle.appendChild(recipeName);
     recipeArticle.appendChild(recipeImage);
-    results.appendChild(recipeArticle);
   });
 }
 
-async function showRecipe() {
-  let receta = await renderRecipe();
+async function menu() {
+  await showStarter();
+  await showMain();
+  await showDrinks();
+  await showDessert();
 }
-showRecipe();
+
+menu();
